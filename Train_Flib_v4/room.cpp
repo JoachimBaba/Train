ba2971f4ff03af87792032997c46b6750682d075
JoachimBaba <joachim.baba@etud.infographie-sup.eu>
2014-09-26 10:23:50 +0200

    creation de room.cpp

---
+++ Train_Flib_v4/room.cpp
@@ -0,0 +1,14 @@
+#include "room.h"
+
+TRoom::TRoom()
+	:m_pDoorRoot(0)
+{
+
+}
+
+/*WITH PARAM
+TRoom::TRoom()
+{
+	m_pDoorRoot = new TSLLRoot < TDoor >;
+}
+*/
--- Train_Flib_v4/room.h
+++ Train_Flib_v4/room.h
@@ -5,7 +5,10 @@ There is one room at the time on the screen.
 The player walk throught the rooms to progress in the game.
 */
 
+#include "flib.h"
 #include "sprite_element.h"
+#include "door.h"
+#include "sll_root.h"
 
 const int WALK_AREA_SIZE = 64;
 const int WALK_AREA_X = WINDOW_SIZE_X / WALK_AREA_SIZE;
@@ -17,9 +20,11 @@ public:
 	TRoom();
 	virtual ~TRoom();
 
+	//void ReadDataToCreateContent()
+
 private:
-	bool m_pWalkzone[WALK_AREA_X][WALK_AREA_Y];
-	//TDoor *m_pRootDoor;
+	//bool m_pWalkzone[WALK_AREA_X][WALK_AREA_Y];
+	TSLLRoot<TDoor> *m_pDoorRoot;
 	//TInteractiveObject *m_pInteractiveObject_Root;
 	//TNonPlayerCharacter *m_pCharacter_Root;
 };
